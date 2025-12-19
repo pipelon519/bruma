dd735b97877474c0a10453c56c93de71456d5548
epilef519 <epilef519@gmail.com>
2025-12-19 19:35:44 +0000

    modified:   src/App.tsx 	modified:   src/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Categories from "./pages/categories";
+import Header from "./pages/components/header";
+import Footer from "./pages/components/footer";
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
-
-export default App;
